e14a6fa186cf71374dbaed89c0e32805b0cec806
Paul-G-Bim <pgbimpro@gmail.com>
2024-12-12 04:33:49 -0400

    Implement "Delete todo item", feature

--- web.py
+++ web.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from streamlit import session_state
+
 import r_w_todos as rw
 
 todos = rw.get_todos()
@@ -13,7 +15,15 @@ st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity.")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        rw.write_todos(todos)
+        del session_state[todo]
+        st.rerun()
 
 st.text_input(label="", placeholder="Enter a new todo...", on_change=add_todo, key="new_todo")
+
+# enable below line used to see the session.state object (dictionary)
+# st.session_state
